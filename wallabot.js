e9af6b7ac86a9ef5a6073fda3278741b0d14e763
Josseph <jossephalvarez@gmail.com>
2020-11-25 16:31:54 +0100

    enhancement(wallaBot.js): human interactive write inputs, some refactor

--- wallabot.js
+++ wallabot.js
@@ -1,26 +1,13 @@
-const { getRandomIntBetween } = require("./utils/funcs");
+const {
+  getRandomIntBetween,
+  shuffleArray,
+  executeWriteInputs,
+} = require("./utils/funcs");
 
 async function createAd(page) {
   await page.evaluate(() => {
     document.querySelector('.types').firstElementChild.click();
   });
-
-  const titleAdsId = '#headline';
-  const titleText = 'Charms Disney 2020 Oferta';
-
-  const priceId = '#price';
-  const priceText = '0';
-
-  const descriptionId = '#tellUs';
-  const descriptionText =
-    `${titleText} Nuevo a Estrenar Envios A toda España . \n` +
-    '100% Plata de ley\n' +
-    '\n' +
-    'Acepto Bizum, transferencia o ingreso.\n' +
-    '\n' +
-    'Precios desde 10 euros. \n' +
-    '\n' +
-    '#CharmDisney #MickeyMouse #MinnieMouse #Charm #IPHONE #IPHONE6 #REGALODEREYES #NAVIDAD';
 
   const writeInputs = async (id, text) => {
     await page.waitForSelector(id);
@@ -28,36 +15,76 @@ async function createAd(page) {
     await page.type(id, text, { delay: getRandomIntBetween(100, 200) });
   };
 
-  await writeInputs(titleAdsId, titleText);
-  await writeInputs(priceId, priceText);
-  await writeInputs(descriptionId, descriptionText);
+  const priceId = '#price';
+  const priceText = '0';
+
+  const titleAdsId = '#headline';
+  const titleText = 'Edicion ALADINO - PULSERA Y CHARMS en PLATA DE LEY';
+
+  const descriptionId = '#tellUs';
+  const hashtags =
+    '#CharmDisney #MickeyMouse #MinnieMouse #Charm #IPHONE #IPHONE6 #REGALODEREYES #NAVIDAD #OUTLET #LIQUIDACION #OFERTAS #ALADINO #FROZEN #MULAN';
+  const descriptionText = `${
+    `${titleText} Nuevo a Estrenar Envios A toda España . \n` +
+    "100% Plata de ley\n" +
+    "\n" +
+    "Acepto Bizum, transferencia o ingreso.\n" +
+    "\n" +
+    "Precios desde 10 euros. \n" +
+    "\n"
+  }${hashtags}`;
+
+  /*  const descriptionText = `${
+    `${titleText}\n` +
+    'TODAS LAS TALLA ADULTO Y NIÑO -Mirar imagen para más informacion. \n' +
+    "\n"
+  }${hashtags}`; */
+
+  const writeTitle = async () => writeInputs(titleAdsId, titleText);
+  const writePrice = async () => writeInputs(priceId, priceText);
+  const writeDescription = async () =>
+    writeInputs(descriptionId, descriptionText);
+
+  // FUNCIONALIDAD DE ALETORIEDAD
+
+  const functionsWriteInputs = [writeTitle, writePrice, writeDescription];
+  const disorderFunctionsWriteInputs = shuffleArray(functionsWriteInputs);
+
+  await executeWriteInputs(disorderFunctionsWriteInputs);
+
+  //  END FUNCIONALIDAD DE ALETORIEDAD
+
   // FILE UPLOAD
   await page.waitForSelector('input[type=file]');
 
   const inputUploadHandle = await page.$('input[type=file]');
 
-  const fileToUploadPrincipal = 'image_principal.jpg';
+  const fileToUploadPrincipal = 'edicion_aladino.jpg';
   const fileToUpload1 = 'image_1.jpg';
   const fileToUpload2 = 'image_2.jpg';
   const fileToUploadGeneral1 = 'image_general_1.jpg';
 
   // Sets the value of the file input to fileToUpload
   inputUploadHandle.uploadFile(fileToUploadPrincipal);
-  inputUploadHandle.uploadFile(fileToUpload1);
+  /* inputUploadHandle.uploadFile(fileToUpload1);
   inputUploadHandle.uploadFile(fileToUpload2);
-  inputUploadHandle.uploadFile(fileToUploadGeneral1);
+  inputUploadHandle.uploadFile(fileToUploadGeneral1); */
+
   // END FILE UPLOAD
 
   // SELECT BOXES
   const categoryId = '#category';
   const categoryText = 'Moda y accesorios';
+
   await writeInputs(categoryId, categoryText);
   await page.keyboard.press('Enter');
 
   const objectTypeId = '#objectType';
   const objectTypeText = 'Pulseras';
+  // const objectTypeText = 'Camiseta Deporte';
   const objectTypeInputSelector =
     '#objectType > div > tsl-dropdown-list > div > div.filter > input';
+
   await page.click(objectTypeId);
   await writeInputs(objectTypeInputSelector, objectTypeText);
   await page.keyboard.press('Enter');
@@ -66,6 +93,7 @@ async function createAd(page) {
   const genderText = 'Mujer';
   const genderInputSelector =
     '#gender > div > tsl-dropdown-list > div > div.filter > input';
+
   await page.click(genderId);
   await writeInputs(genderInputSelector, genderText);
   await page.keyboard.press('Enter');
@@ -74,6 +102,7 @@ async function createAd(page) {
   const conditionsText = 'Sin estrenar';
   const conditionInputSelector =
     '#conditions > div > tsl-dropdown-list > div > div.filter > input';
+
   await page.click(conditionId);
   await writeInputs(conditionInputSelector, conditionsText);
   await page.keyboard.press('Enter');
@@ -87,6 +116,5 @@ async function createAd(page) {
     document.querySelector('.selector-container').firstElementChild.click();
   });
   await page.keyboard.press('Enter');
-  await page.screenshot({ path: 'product_done_before.jpg' });
 }
 module.exports = { createAd };
