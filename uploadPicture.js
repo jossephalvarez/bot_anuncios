48f3903194fe197b27b8a66e2977c982d9f8ed0e
Josseph <jossephalvarez@gmail.com>
2020-12-09 17:43:47 +0100

    enhancement(uploadPicture.js): support multiples images

--- uploadPicture.js
+++ uploadPicture.js
@@ -3,16 +3,17 @@ const { getRandomIntBetween, delay } = require('./utils/funcs');
 async function uploadPicture(page, images) {
   await page.waitForSelector('input[type=file]');
 
-  const inputUploadHandle = await page.$('input[type=file]');
-
   if (images && images.length > 0) {
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < images.length; i++) {
+      // eslint-disable-next-line no-await-in-loop
+      const inputUploadHandle = await page.$('input[type=file]');
       const fileToUploadPrincipal = images[i];
       // Sets the value of the file input to fileToUpload
-      inputUploadHandle.uploadFile(fileToUploadPrincipal);
       // eslint-disable-next-line no-await-in-loop
-      await delay(getRandomIntBetween(450, 1000));
+      await inputUploadHandle.uploadFile(fileToUploadPrincipal);
+      // eslint-disable-next-line no-await-in-loop
+      await delay(getRandomIntBetween(450, 1500));
     }
   }
 
